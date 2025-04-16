94fabd07f4eba703c9d85dbb3b8123ef77b1c78e
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2025-04-16 10:23:23 +0200

    feat: integrate dompurify for sanitization and enhance AnimatedTitle component with GSAP animations

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -32,7 +32,10 @@ function About() {
     <div id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
         <h2 className="font-general text-sm uppercase">Welcome to Zentry</h2>
-        <AnimatedTitle />
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
+          containerClass="mt-5 !text-black text-center"
+        />
         <div className="about-subtext">
           <p>The Game of Games begins-your life, now an epic MMORPG</p>
           <p>Zentry unites every player from coatless games and platforms</p>
--- src/components/animated-title.tsx
+++ src/components/animated-title.tsx
@@ -1,13 +1,59 @@
-function AnimatedTitle() {
+import dompurify from "dompurify";
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
+
+type Props = {
+  title: string;
+  containerClass: string;
+};
+
+function AnimatedTitle({ title, containerClass }: Props) {
+  const sanitizer = dompurify.sanitize;
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      const titleAnimation = gsap.timeline({
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "100 bottom",
+          end: "center bottom",
+          toggleActions: "play none none reverse",
+        },
+      });
+
+      titleAnimation.to(
+        ".animated-word",
+        {
+          opacity: 1,
+          transform: "translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg)",
+          ease: "power2.inOut",
+          stagger: 0.02,
+        },
+        0,
+      );
+    }, containerRef);
+
+    return () => ctx.revert(); // Clean up on unmount
+  }, []);
+
   return (
-    <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-      Disc
-      <b>o</b>
-      ver the world's
-      <br />
-      l
-      <b>a</b>
-      rgest shared
+    <div ref={containerRef} className={`animated-title ${containerClass}`}>
+      {title.split("<br />").map(line => (
+        <div
+          key={`${line.trim}-${Math.random()}`}
+          className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3"
+        >
+          {line.split(" ").map(word => (
+            <span
+              key={`${sanitizer(word)}-${Math.random()}`}
+              className="animated-word"
+              // eslint-disable-next-line react-dom/no-dangerously-set-innerhtml
+              dangerouslySetInnerHTML={{ __html: sanitizer(word) }}
+            />
+          ))}
+        </div>
+      ))}
     </div>
   );
 }

87d0947c16b6a17dce94cc5e48c4889f74c0f594
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2025-04-16 11:10:02 +0200

    feat: add animated navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import About from "./components/about";
 import Hero from "./components/hero";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
       <Hero />
       <About />
     </main>

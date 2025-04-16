3fd7802ce206621b65667d22b9cb2b8079610acf
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2025-04-16 09:51:25 +0200

    feat: add AnimatedTitle component and integrate into About section

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -2,6 +2,8 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 
+import AnimatedTitle from "./animated-title";
+
 ;
 
 function About() {
@@ -30,15 +32,7 @@ function About() {
     <div id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
         <h2 className="font-general text-sm uppercase">Welcome to Zentry</h2>
-        <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-          Disc
-          <b>o</b>
-          ver the world's
-          <br />
-          l
-          <b>a</b>
-          rgest shared
-        </div>
+        <AnimatedTitle />
         <div className="about-subtext">
           <p>The Game of Games begins-your life, now an epic MMORPG</p>
           <p>Zentry unites every player from coatless games and platforms</p>
